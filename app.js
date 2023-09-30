e981b928a244f2a800ad2a34be21d9e02dc903fd
Umair <uak1911@gmail.com>
2023-09-30 15:32:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+
+app.listen(3000,function(){
+    console.log("everything is working fine");
+});
