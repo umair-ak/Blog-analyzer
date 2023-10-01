576c92412ea5ce045fb715c09649408bd4c234bc
Umair <uak1911@gmail.com>
2023-10-01 14:12:39 +0530

    analysis complete

---
+++ analysis.js
@@ -0,0 +1,53 @@
+const _ = require("lodash");
+
+// returns the total number of blogs.
+function totalNoOfBlogs(allBlogs){
+    return _.size(allBlogs);
+}
+
+// Find the longest title among all the blogs.
+function longestBlog(allBlogs){
+    let longest = "";
+    console.log(allBlogs[0]);
+    allBlogs.forEach(blog => {
+        let title = blog.title;
+        if(_.size(title)>_.size(longest)){
+            longest = title;
+        }        
+    });
+    return longest;
+}
+
+// function to determine the number of blogs that contain the word privacy 
+function titlesWithPrivacy(allBlogs){
+    let count = 0;
+
+    allBlogs.forEach(blog => {
+        let title = _.lowerCase(blog.title);
+
+        if(title.includes("privacy")){
+            count++;
+        }
+    });
+    return count;
+}
+
+// return an array that contain all the unique blog titles.
+function findUinque(allBlogs){
+
+    let titlesArr = []
+    
+    allBlogs.forEach(blog => {
+        let title = _.lowerCase(blog.title);
+        titlesArr.push(title);
+    });
+
+    return Array.from(new Set(titlesArr));
+}
+
+module.exports = {
+    totalNoOfBlogs,
+    longestBlog,
+    titlesWithPrivacy,
+    findUinque,
+};
--- routes/blogstats.js
+++ routes/blogstats.js
@@ -1,18 +1,34 @@
 require('dotenv').config();
 const express = require("express");
 const axios = require("axios");
+const analyser = require("./../analysis");
 const router = express.Router();
 
 const blog_url = "https://intent-kit-16.hasura.app/api/rest/blogs";
 
+function analyse(Blogs){
+    console.log("Number of blogs fetched : "+analyser.totalNoOfBlogs(Blogs));
+    
+    console.log(analyser.longestBlog(Blogs));
+
+    console.log("no of blogs containing word privacy : "+analyser.titlesWithPrivacy(Blogs));
+    
+    let uniqueTitles = analyser.findUinque(Blogs);
+    
+};
+
+
 router.get("/",async (req,res)=>{
     try {
-        const data = await axios.get(blog_url,{
+        const result = await axios.get(blog_url,{
             headers:{
                 'x-hasura-admin-secret':process.env.KEY
             }
         });
-        console.log(data.data.blogs);
+        let  Blogs = result.data.blogs
+        analyse(Blogs);
+        
+        res.send("<h1>data is fetched.</h1>");
         }catch (error) {
             console.log(error);
         }
